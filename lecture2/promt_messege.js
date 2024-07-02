fffcaac03397279463b34e86fde3f8169cd1df2b
Surajchy2055 <surajchy2055@gmail.com>
2024-07-02 20:14:38 +0530

    prompt message

---
+++ lecture2/promt_messege.js
@@ -0,0 +1,25 @@
+// Get user to input a number using prompt("Enter a number:").Check if the number is a multiple of 5 and 3 or not.
+
+
+// let num = prompt("Enter a number");
+// if ( num % 5 === 0)
+//     {
+//         console.log(num,"is multiple of 5");
+//     }
+// else{
+//     console.log(num,"is not multiple of 5");
+// }
+
+// console.log(num);
+
+
+let number = prompt("Enter a number");
+if ( number % 3 === 0)
+    {
+        console.log(number,"is multiple of 3");
+    }
+else{
+    console.log(number,"is not multiple of 3");
+}
+
+console.log(number);
