a1e90caa18af7562b53b85adf20ffa1639f3c107
Surajchy2055 <surajchy2055@gmail.com>
2024-07-02 19:29:54 +0530

    conditional statement completed

--- lecture2/conditional_statement.js
+++ lecture2/conditional_statement.js
@@ -22,13 +22,52 @@
 // console.log(color);
 
 // if else statement
-let mode = "gray";
-let color;
- if( mode==="dark"){
-    color="black";
+// let mode = "gray";
+// let color;
+//  if( mode==="dark"){
+//     color="black";
 
- }
- else{
-    color = " white"
- }
- console.log(color);
+//  }
+//  else{
+//     color = " white"
+//  }
+//  console.log(color);
+
+//  let age = 15;
+//  if( age < 18){
+//     console.log("you cannot vote");
+//  }
+//  else{
+//     console.log("you can vote");
+//  }
+
+//  let num = 24;
+//  if ( num % 2 === 1){
+//     console.log( num,"the num is odd");
+//  }
+//  else{
+//     console.log(num,"the number is even");
+//  }
+
+// else if statement
+// let age = 80;
+// if (age <= 18)
+//    {
+//       console.log("Teenager");
+//    }
+// else if(age <=40)
+//    {
+//       console.log("younger");
+//    }
+// else if(age <=60)
+//    {
+//       console.log("elder");
+//    }
+// else{
+//    console.log("older");
+// }
+// Ternary Operator
+// let age = 15;
+// let result = age >= 18 ? "Adult":"Teenager";
+// console.log(result);
+
